d4e42e8565d9e006dedc19ac8bf118d7243d0290
Anierin Bliss <Anierin.T.Bliss@gmail.com>
2019-11-20 10:55:08 +0000

    fajita: Evo-Xify
    Signed-off-by: AnierinB <anierinb@evolution-x.org>

--- aosp_fajita.mk
+++ aosp_fajita.mk
@@ -21,10 +21,24 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from fajita device
 $(call inherit-product, device/oneplus/fajita/device.mk)
 
-# Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common Evolution X stuff.
+$(call inherit-product, vendor/aosp/config/common_full_phone.mk)
 
-PRODUCT_NAME := lineage_fajita
+# Official
+CUSTOM_BUILD_TYPE := OFFICIAL
+
+# Boot Animation
+TARGET_BOOT_ANIMATION_RES := 1080
+
+# Gapps
+TARGET_GAPPS_ARCH := arm64
+TARGET_INCLUDE_WIFI_EXT := true
+
+# TWRP
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.product.device=OnePlus6T
+
+PRODUCT_NAME := aosp_fajita
 PRODUCT_DEVICE := fajita
 PRODUCT_MANUFACTURER := OnePlus
 PRODUCT_BRAND := OnePlus
