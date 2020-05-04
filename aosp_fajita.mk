94d027392a9fb095e725389e0636ec5aa6b7b8bf
Joey Huab <joey@evolution-x.org>
2020-05-04 18:06:49 +0000

    fajita: Update to Coral's May 2020 build fingerprint
    Signed-off-by: AnierinB <anierinb@evolution-x.org>

--- aosp_fajita.mk
+++ aosp_fajita.mk
@@ -51,4 +51,4 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=OnePlus6T \
     PRIVATE_BUILD_DESC="OnePlus6T-user 9 PKQ1.180716.001 1812260627 release-keys"
 
-BUILD_FINGERPRINT := google/coral/coral:10/QQ2A.200405.005/6254899:user/release-keys
+BUILD_FINGERPRINT := google/coral/coral:10/QQ2A.200501.001.B2/6352890:user/release-keys
