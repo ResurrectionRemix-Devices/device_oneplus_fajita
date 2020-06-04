06dd820b16f14578eae10437b952319f8182ebf8
AnierinB <anierinb@evolution-x.org>
2020-06-04 08:53:06 +0000

    fajita: Remove useless boot animation flags
    -  https://github.com/Evolution-X-Devices/device_oneplus_fajita/blob/54b6a47b2961f88ee566d5bb0dd2e7b9028c74c5/aosp_fajita.mk#L31
    
    Signed-off-by: AnierinB <anierinb@evolution-x.org>

--- device.mk
+++ device.mk
@@ -36,10 +36,6 @@ PRODUCT_COPY_FILES += \
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xxhdpi
 
-# Boot animation
-TARGET_SCREEN_HEIGHT := 2340
-TARGET_SCREEN_WIDTH := 1080
-
 # Fingerprint
 PRODUCT_PACKAGES += \
     lineage.biometrics.fingerprint.inscreen@1.0-service.oneplus_fajita \
